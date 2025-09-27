290dd9db55121b74f9b6904aa3559431a386ffc1
Torben Halvorsen Rygg <torben.rygg@gmail.com>
2025-09-27 14:26:47 +0200

    Use root base for custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pentangelen-site/', // repo-navnet ditt her
-})
+  base: "/", // <- var /pentangelen-site/
+});
